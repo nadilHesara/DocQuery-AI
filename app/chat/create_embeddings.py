6658f402cc52db3f20b9526420bc016929f46978
nadilHesara <nadilhesara@gmail.com>
2026-01-16 23:20:34 +0530

    created embeddings and interconnected langchain with pinecona and openai. Tested

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from app.chat.vector_stores.pinecone import vector_store
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     """
@@ -25,6 +26,6 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
 
-    print(repr(docs))
+    vector_store.add_documents(docs)
 
     
