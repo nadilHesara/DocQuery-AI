1806b063638e10a78fca54991799d1cccb5ec612
nadilHesara <nadilhesara@gmail.com>
2026-01-17 15:44:56 +0530

    Streaming chain works perfectly.

--- test.py
+++ test.py
@@ -3,12 +3,16 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
+from queue import Queue
+from threading import Thread
 
 load_dotenv()
 
+queue = Queue()
+
 class StreamingHandler(BaseCallbackHandler):
     def on_llm_new_token(self, token, **kwargs):
-        pass
+        queue.put(token)
 
 chat = ChatOpenAI(
     streaming=True,
@@ -21,9 +25,14 @@ prompt = ChatPromptTemplate.from_messages([
 
 class StreamingChain(LLMChain):
     def stream(self, input):
-        print(self(input))
-        yield 'hi'
-        yield 'there'
+        def task():
+            self(input)
+
+        Thread(target=task).start()
+
+        while True:
+            token = queue.get()
+            yield token
 
 chain = StreamingChain(llm=chat, prompt=prompt)
 
