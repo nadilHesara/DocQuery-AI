3f4ba73998cf675bd4e579121780ff7d136cead6
nadilHesara <nadilhesara@gmail.com>
2026-01-17 17:48:22 +0530

    Streming errors fixed. All set in streaming. Tested.

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,3 +1,4 @@
+from langchain_openai import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
@@ -18,10 +19,12 @@ def build_chat(chat_args: ChatArgs):
 
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
+        condense_question_llm=condense_question_llm,
         memory=memory,
         retriever=retriever
     )
