da7bb4cab59776738c004617acb37de0371d2bcb
nadilHesara <nadilhesara@gmail.com>
2026-01-17 15:12:52 +0530

    Used streaming handlers to recieve chunks with a callback

---
+++ test.py
@@ -0,0 +1,30 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class StreamingHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token, **kwargs):
+        print(token)
+
+chat = ChatOpenAI(
+    streaming=True,
+    callbacks=[StreamingHandler()]
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("human", "{content}")
+])
+chain = LLMChain(llm=chat, prompt=prompt)
+
+for output in chain.stream(input={"content":"Tell me a joke"}):
+    print(output)
+
+""" messages = prompt.format_messages(content="Tell me a joke")
+
+for message in chat.stream(messages):
+    print(message.content)
+ """
