110077542f588c35875d2cb208a103d7b4f27797
nadilHesara <nadilhesara@gmail.com>
2026-01-19 10:16:44 +0530

    Randomized component pickings

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,9 +1,14 @@
+import random
 from langchain_openai import ChatOpenAI
 from app.chat.models import ChatArgs
-from app.chat.vector_stores.pinecone import build_retriever
+from app.chat.vector_stores.pinecone import retriever_map
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
+from app.web.api import (
+    set_conversation_components,
+    get_conversation_components
+)
 
 def build_chat(chat_args: ChatArgs):
     """
@@ -17,7 +22,26 @@ def build_chat(chat_args: ChatArgs):
         chain = build_chat(chat_args)
     """
 
-    retriever = build_retriever(chat_args)
+    components = get_conversation_components(
+        chat_args.conversation_id
+    )
+    previous_retriever = components["retriever"]
+
+    retriever = None
+    if previous_retriever:
+        build_retriever = retriever_map[previous_retriever]
+        retriever = build_retriever(chat_args)
+    else:
+        random_retriever_name = random.choice(list(retriever_map.keys()))
+        build_retriever = retriever_map[random_retriever_name]
+        retriever = build_retriever(chat_args)
+        set_conversation_components(
+            conversation_id=chat_args.conversation_id,
+            llm="",
+            memory="",
+            retriever=random_retriever_name
+        )
+
     llm = build_llm(chat_args)
     condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
